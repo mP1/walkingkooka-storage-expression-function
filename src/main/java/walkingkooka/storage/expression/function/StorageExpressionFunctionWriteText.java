2ade05e726194661088e51a991239aa185f344ac
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-03-26 19:59:10 +1100

    StorageExpressionFunctionWriteText
    - Closes https://github.com/mP1/walkingkooka-storage-expression-function/issues/5
    - ExpressionFunction: StorageWriteText(StoragePath, String text, String encoding)

---
+++ src/main/java/walkingkooka/storage/expression/function/StorageExpressionFunctionWriteText.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.storage.expression.function;
+
+import walkingkooka.Cast;
+import walkingkooka.storage.StoragePath;
+import walkingkooka.storage.StorageValue;
+import walkingkooka.tree.expression.function.ExpressionFunctionParameter;
+import walkingkooka.tree.expression.function.ExpressionFunctionParameterKind;
+import walkingkooka.tree.expression.function.ExpressionFunctionParameterName;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Writes some text to {@link walkingkooka.storage.StorageStore}.
+ */
+final class StorageExpressionFunctionWriteText<C extends StorageExpressionEvaluationContext> extends StorageExpressionFunction<C, Void> {
+
+    /**
+     * Type safe getter.
+     */
+    static <C extends StorageExpressionEvaluationContext> StorageExpressionFunctionWriteText<C> instance() {
+        return Cast.to(INSTANCE);
+    }
+
+    final static StorageExpressionFunctionWriteText INSTANCE = new StorageExpressionFunctionWriteText<>();
+
+    private StorageExpressionFunctionWriteText() {
+        super("StorageWriteText");
+    }
+
+    @Override
+    public List<ExpressionFunctionParameter<?>> parameters(final int count) {
+        return PARAMETERS;
+    }
+
+    private final static ExpressionFunctionParameter<String> TEXT = ExpressionFunctionParameterName.with("text")
+        .required(String.class)
+        .setKinds(ExpressionFunctionParameterKind.CONVERT_EVALUATE_RESOLVE_REFERENCES);
+
+    final static List<ExpressionFunctionParameter<?>> PARAMETERS = ExpressionFunctionParameter.list(
+        PATH,
+        TEXT
+    );
+
+    @Override
+    public Class<Void> returnType() {
+        return Void.class;
+    }
+
+    @Override
+    public Void apply(final List<Object> parameters,
+                      final C context) {
+        final StoragePath path = PATH.getOrFail(
+            parameters,
+            0
+        );
+
+        final String text = TEXT.getOrFail(
+            parameters,
+            1
+        );
+
+        context.storage()
+            .save(
+                StorageValue.with(
+                    path,
+                    Optional.of(text)
+                )
+            );
+
+        return null;
+    }
+}
