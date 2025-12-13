ca21b01d89fc943dcf25d5cb20e7661ba63df7e6
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-12-13 11:00:42 +1100

    https://github.com/mP1/walkingkooka-tree/pull/995 BasicNodeSelectorExpressionEvaluationContextReferenceFunction removed
    - https://github.com/mP1/walkingkooka-tree/pull/995
    - BasicNodeSelectorExpressionEvaluationContextReferenceFunction removed

--- src/main/java/walkingkooka/storage/expression/function/StorageExpressionEvaluationContextDelegator.java
+++ src/main/java/walkingkooka/storage/expression/function/StorageExpressionEvaluationContextDelegator.java
@@ -17,13 +17,58 @@
 
 package walkingkooka.storage.expression.function;
 
+import walkingkooka.environment.EnvironmentValueName;
+import walkingkooka.net.email.EmailAddress;
 import walkingkooka.storage.Storage;
+import walkingkooka.text.LineEnding;
 import walkingkooka.tree.expression.ExpressionEvaluationContext;
 import walkingkooka.tree.expression.ExpressionEvaluationContextDelegator;
 
-public interface StorageExpressionEvaluationContextDelegator extends StorageExpressionEvaluationContext, ExpressionEvaluationContextDelegator{
+import java.util.Locale;
+import java.util.Optional;
+
+public interface StorageExpressionEvaluationContextDelegator extends StorageExpressionEvaluationContext, ExpressionEvaluationContextDelegator {
 
     // ExpressionEvaluationContextDelegator.............................................................................
+
+    @Override
+    default <T> StorageExpressionEvaluationContext setEnvironmentValue(final EnvironmentValueName<T> name,
+                                                                       final T value) {
+        this.environmentContext()
+            .setEnvironmentValue(
+                name,
+                value
+            );
+        return this;
+    }
+
+    @Override
+    default StorageExpressionEvaluationContext removeEnvironmentValue(EnvironmentValueName<?> name) {
+        this.environmentContext()
+            .removeEnvironmentValue(name);
+        return this;
+    }
+
+    @Override
+    default StorageExpressionEvaluationContext setLineEnding(final LineEnding lineEnding) {
+        this.environmentContext()
+            .setLineEnding(lineEnding);
+        return this;
+    }
+
+    @Override
+    default StorageExpressionEvaluationContext setLocale(final Locale locale) {
+        this.environmentContext()
+            .setLocale(locale);
+        return this;
+    }
+
+    @Override
+    default StorageExpressionEvaluationContext setUser(final Optional<EmailAddress> user) {
+        this.environmentContext()
+            .setUser(user);
+        return this;
+    }
 
     @Override
     default ExpressionEvaluationContext expressionEvaluationContext() {
