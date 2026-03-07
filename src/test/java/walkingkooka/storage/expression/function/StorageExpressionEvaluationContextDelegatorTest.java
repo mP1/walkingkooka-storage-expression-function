55dd453f2531ba88c744cd9f481ab28cd1b8456b
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-03-07 18:37:08 +1100

    https://github.com/mP1/walkingkooka-environment/pull/330 EnvironmentContext.addEnvironmentValueWatcherXXX was addEventValueWatcherXXX
    - https://github.com/mP1/walkingkooka-environment/pull/330
    - EnvironmentContext.addEnvironmentValueWatcherXXX was addEventValueWatcherXXX

--- src/test/java/walkingkooka/storage/expression/function/StorageExpressionEvaluationContextDelegatorTest.java
+++ src/test/java/walkingkooka/storage/expression/function/StorageExpressionEvaluationContextDelegatorTest.java
@@ -502,13 +502,13 @@ public final class StorageExpressionEvaluationContextDelegatorTest implements St
         private final EnvironmentContext environmentContext;
 
         @Override
-        public Runnable addEventValueWatcher(final EnvironmentValueWatcher watcher) {
+        public Runnable addEnvironmentValueWatcher(final EnvironmentValueWatcher watcher) {
             Objects.requireNonNull(watcher, "watcher");
             throw new UnsupportedOperationException();
         }
 
         @Override
-        public Runnable addEventValueWatcherOnce(final EnvironmentValueWatcher watcher) {
+        public Runnable addEnvironmentValueWatcherOnce(final EnvironmentValueWatcher watcher) {
             Objects.requireNonNull(watcher, "watcher");
             throw new UnsupportedOperationException();
         }
