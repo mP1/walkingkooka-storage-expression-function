543abf7209e985d753e6c169d54fa02393407afc
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-08-10 09:59:49 +1000

    StorageExpressionFunctionUnmount
    - Closes https://github.com/mP1/walkingkooka-storage-expression-function/issues/163
    - ExpressionFunction: unmount

---
+++ src/main/java/walkingkooka/storage/expression/function/StorageExpressionFunctionUnmount.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.storage.expression.function;
+
+import walkingkooka.Cast;
+import walkingkooka.storage.StoragePath;
+import walkingkooka.tree.expression.function.ExpressionFunctionParameter;
+
+import java.util.List;
+
+/**
+ * Unmounts a mount given its {@link StoragePath}.
+ */
+final class StorageExpressionFunctionUnmount<C extends StorageExpressionEvaluationContext> extends StorageExpressionFunction<C, Void> {
+
+    /**
+     * Type safe getter.
+     */
+    static <C extends StorageExpressionEvaluationContext> StorageExpressionFunctionUnmount<C> instance() {
+        return Cast.to(INSTANCE);
+    }
+
+    final static StorageExpressionFunctionUnmount<?> INSTANCE = new StorageExpressionFunctionUnmount<>();
+
+    private StorageExpressionFunctionUnmount() {
+        super("unmountStorage");
+    }
+
+    @Override
+    public List<ExpressionFunctionParameter<?>> parameters(final int count) {
+        return PARAMETERS_PATH_REQUIRED;
+    }
+
+    @Override
+    public Class<Void> returnType() {
+        return Void.class;
+    }
+
+    @Override
+    public Void apply(final List<Object> parameters,
+                      final C context) {
+        final StoragePath path = PATH_REQUIRED.getOrFail(
+            parameters,
+            0
+        );
+
+        context.unmountStorage(path);
+
+        return null;
+    }
+}
---
+++ src/test/java/walkingkooka/storage/expression/function/StorageExpressionFunctionUnmountTest.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.storage.expression.function;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.Cast;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.storage.Storage;
+import walkingkooka.storage.StorageMountPoint;
+import walkingkooka.storage.StoragePath;
+import walkingkooka.storage.StorageTesting;
+import walkingkooka.storage.Storages;
+import walkingkooka.storage.expression.function.StorageExpressionFunctionTestCase.TestStorageExpressionEvaluationContext;
+
+public final class StorageExpressionFunctionUnmountTest extends StorageExpressionFunctionTestCase<StorageExpressionFunctionUnmount<TestStorageExpressionEvaluationContext>, Void>
+    implements StorageTesting {
+
+    private final static StoragePath PATH = StoragePath.parse("/mount1/");
+
+    @Test
+    public void testApplyUnmount() {
+        final Storage<TestStorageExpressionEvaluationContext> storage = Storages.mount(
+            Storages.treeMapStore()
+        );
+        final TestStorageExpressionEvaluationContext context = this.createContext(storage);
+
+        storage.mount(
+            StorageMountPoint.with(
+                PATH,
+                Storages.treeMapStore()
+            ),
+            context
+        );
+
+        this.applyAndCheck(
+            StorageExpressionFunctionUnmount.instance(),
+            Lists.of(PATH),
+            context,
+            null
+        );
+
+        this.mountPointsAndCheck(
+            storage
+        );
+    }
+
+    @Override
+    public StorageExpressionFunctionUnmount<TestStorageExpressionEvaluationContext> createBiFunction() {
+        return StorageExpressionFunctionUnmount.instance();
+    }
+
+    @Override
+    public TestStorageExpressionEvaluationContext createContext() {
+        return this.createContext(Storages.empty());
+    }
+
+    private TestStorageExpressionEvaluationContext createContext(final Storage<TestStorageExpressionEvaluationContext> storage) {
+        return new TestStorageExpressionEvaluationContext(storage) {
+
+            @Override
+            public void unmountStorage(final StoragePath path) {
+                storage.unmount(
+                    path,
+                    this
+                );
+            }
+        };
+    }
+
+    @Override
+    public int minimumParameterCount() {
+        return 1;
+    }
+
+    // toString.........................................................................................................
+
+    @Test
+    public void testToString() {
+        this.toStringAndCheck(
+            StorageExpressionFunctionUnmount.instance(),
+            "unmountStorage"
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<StorageExpressionFunctionUnmount<TestStorageExpressionEvaluationContext>> type() {
+        return Cast.to(StorageExpressionFunctionUnmount.class);
+    }
+}
